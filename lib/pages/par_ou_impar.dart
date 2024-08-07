2f4fceef7b1b39b6b57feae92d2eab053b1f1913
Dione Santos <dionesantos@Diones-MacBook-Pro.local>
2024-08-06 21:21:24 -0300

    feat: app par ou impar finalizado

--- lib/pages/par_ou_impar.dart
+++ lib/pages/par_ou_impar.dart
@@ -8,6 +8,27 @@ class ParOuImpar extends StatefulWidget {
 }
 
 class _ParOuImparState extends State<ParOuImpar> {
+  TextEditingController inputController = TextEditingController();
+  String resultado = '';
+
+  verificaParOuImpar() {
+    setState(() {
+      final numeroDigitado = int.tryParse(inputController.text);
+
+      if (numeroDigitado != null) {
+        if (numeroDigitado % 2 == 0) {
+          resultado = 'Par';
+        } else {
+          resultado = 'Impar';
+        }
+      } else {
+        resultado = 'Por favor, digite um número.';
+      }
+
+      inputController.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,28 +43,34 @@ class _ParOuImparState extends State<ParOuImpar> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Par/Impar',
-                  style: TextStyle(fontSize: 70),
+                  resultado,
+                  style: TextStyle(
+                    fontSize:
+                        resultado == 'Par' || resultado == 'Impar' ? 70 : 18,
+                  ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 50, bottom: 20),
+                  padding: const EdgeInsets.only(top: 50, bottom: 20),
                   child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
+                    controller: inputController,
+                    decoration: const InputDecoration(
                       labelText: 'Número',
                       border: OutlineInputBorder(),
                     ),
+                    keyboardType: TextInputType.number,
                   ),
                 ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    verificaParOuImpar();
+                  },
                   style: ElevatedButton.styleFrom(
                     fixedSize: const Size(270, 48),
                   ),
                   child: const Text(
                     'Verificar',
                     style: TextStyle(
-                      fontSize: 16,
+                      fontSize: 18,
                       color: Color.fromRGBO(45, 44, 44, 1),
                       fontWeight: FontWeight.w600,
                     ),
